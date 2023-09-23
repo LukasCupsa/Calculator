4298b2a53ae23a89397091a1880861ab64a309b6
Lukas Cupsa <144720858+LukasCupsa@users.noreply.github.com>
2023-09-22 20:36:40 -0400

    Update OperationButton.js

--- OperationButton.js
+++ OperationButton.js
@@ -1,3 +1,5 @@
+//NOTE: This could be one possible source of error. I am confident that this could it perfect, however the currentOperand feature doe scall this button so potential source?
+
 import { ACTIONS } from "./App"
 
 export default function OperationButton({dispatch, operation}) {
